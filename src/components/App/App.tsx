f02b725a373dc79a4fc8f5c873fa0f155b9c7466
vovangrk <vovangrk@ukr.net>
2026-07-06 23:41:14 +0300

    next step 7

---
+++ src/components/App/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
+import css from "./App.module.css";
+
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import VoteStats from "../VoteStats/VoteStats.tsx";
+
+import "modern-normalize";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({ ...prevVotes, [type]: prevVotes[type] + 1 }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo></CafeInfo>
+        <VoteOptions
+          onVote={handleVote}
+          onReset={resetVotes}
+          canReset={true}
+        ></VoteOptions>
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        ></VoteStats>
+      </div>
+    </>
+  );
+}
+
+export default App;
